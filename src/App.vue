23f45a71ebb8686a5cde875b6ba219b1b4051dc1
Daniel Hauge <Animcuil@gmail.com>
2018-10-03 18:07:49 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+      <h1>{{title}}</h1>
+      <img src=./assets/wowlogo.png>
+      <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      title: 'Warpress'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffd700;
+  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+}
+h1{
+  font-weight: bolder;
+}
+</style>
